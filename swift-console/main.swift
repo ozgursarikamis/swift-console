c629b59d3dfa0b1916a17d6394ecb3978a962f0b
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-04-10 03:05:25 +0300

    Initial Commit

---
+++ swift-console/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-console
+//
+//  Created by Özgür Sarıkamış on 10.04.2020.
+//  Copyright © 2020 Özgür Sarıkamış. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
