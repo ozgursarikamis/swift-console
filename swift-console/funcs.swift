595201b0e839a7e2bb9e16cec137a253f245a623
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-04-12 00:05:36 +0300

    function takes function as a parameter

--- swift-console/funcs.swift
+++ swift-console/funcs.swift
@@ -4,3 +4,42 @@
 //
 
 import Foundation
+
+func greet() -> String {
+    return "hello özgür";
+}
+
+func greet(name: String, on day: String) -> String {
+    return "Hello \(name), today is \(day)";
+}
+
+// return multiple values:
+func calculateStats(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+    var min = scores[0]
+    var max = scores[0]
+    var sum = 0
+
+    for score in scores {
+        if score > max {
+            max = score
+        } else if score < min {
+            min = score
+        }
+        sum += score
+    }
+    return (min, max, sum)
+}
+
+// Function takes a function as a parameter:
+
+func hasAnyMatches(list: [Int], condition: (Int) -> Bool) -> Bool {
+    for item in list {
+        if condition(item) { return true }
+    }
+    return false
+}
+
+func lessThanTen(number: Int) -> Bool {
+    return number < 10;
+}
+
--- swift-console/main.swift
+++ swift-console/main.swift
@@ -8,31 +8,6 @@
 
 import Foundation
 
-let nickName: String? = nil;
-let fullName: String = "John Appleseed"
-let informalGreeting = "Ji \(String(describing: nickName))"
-print(informalGreeting)
-
-let arr = [1, 2, 3, 4];
-print(arr);
-
-for i in arr {
-    print(i)
-}
-
-let vegetable = "red pepper"
-
-switch vegetable {
-case "celery":
-    print("onion")
-case "cucumber", "watercress":
-    print("potato")
-case let x where x.hasSuffix("pepper"):
-    print("is it spicy? \(x)")
-default:
-    print("nothing above")
-}
-
 let interestingNumbers = [
     "Prime": [2, 3, 5, 7, 11, 13],
     "Fibionacci": [1, 1, 2, 3, 5, 8],
@@ -43,36 +18,11 @@ for (kind, numbers) in interestingNumbers {
     print(kind, numbers);
 }
 
-func greet() -> String {
-    return "hello özgür";
-}
-
-func greet(name: String, on day: String) -> String {
-    return "Hello \(name), today is \(day)";
-}
-
-print(greet())
-var g = greet(name: "Özgür", on: "Saturday")
-print(g);
-
-
-// return multiple values:
-func calculateStats(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
-    var min = scores[0]
-    var max = scores[0]
-    var sum = 0
-
-    for score in scores {
-        if score > max {
-            max = score
-        } else if score < min {
-            min = score
-        }
-        sum += score
-    }
-    return (min, max, sum)
-}
 
 let stats = calculateStats(scores: [5, 3, 100, 3, 9])
 print(stats)
+//
 
+let numbers = [20, 19, 7, 12]
+let result = hasAnyMatches(list: numbers, condition: lessThanTen)
+print(result)
