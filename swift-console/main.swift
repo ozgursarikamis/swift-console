2348ce8bdb91c08b61a4a5e3df4f6e9e27bc4279
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-04-12 17:25:04 +0300

    Enums

--- swift-console/main.swift
+++ swift-console/main.swift
@@ -8,81 +8,28 @@
 
 import Foundation
 
-class NamedShape {
-    var numberOfSides: Int = 0
-    var name: String;
-
-    init(name: String) {
-        self.name = name;
-    }
+enum Rank: Int {
+    case ace = 1
+    case two, three, four, five, six, seven, eight, nine, ten
+    case jack, queen, king
 
     func simpleDesc() -> String {
-        return "A sahpe with \(numberOfSides) sides"
-    }
-}
-
-class Square : NamedShape {
-    var sideLength: Double = 0;
-    init(sideLength: Double, name: String) {
-        self.sideLength = sideLength;
-        super.init(name: name);
-        numberOfSides = 4;
-    }
-
-    func area() -> Double {
-        return sideLength * sideLength;
-    }
-
-    override func simpleDesc() -> String {
-        return "a square with sides of length \(sideLength)"
-    }
-}
-
-class ETriangle: NamedShape {
-    var sideLength: Double = 0;
-
-    init(sideLength: Double, name: String) {
-        self.sideLength = sideLength;
-        super.init(name: name)
-        numberOfSides = 3;
-    }
-
-    var perimeter: Double {
-        get {
-            3.0 * sideLength;
-        } set {
-            sideLength = newValue / 3.0;
+        switch self {
+        case .ace:
+            return "ace"
+        case .jack:
+            return "jack"
+        case .queen:
+            return "queen"
+        case .king:
+            return "king"
+        default:
+            return String(self.rawValue)
         }
     }
-    override func simpleDesc() -> String {
-        return "an equilateral triangle with sides of length \(sideLength)";
-    }
 }
 
-class TriangleAndSquare {
-    /*
-    If you don’t need to compute the property but still need to provide code that is run before and after setting a new value,
-    use willSet and didSet. The code you provide is run any time the value changes outside of an initializer.
-    For example, the class below ensures that the side length of its triangle is always the same as the side length of its square.
-    */
-    var triangle: ETriangle {
-        willSet { // before setter is triggered
-            square.sideLength = newValue.sideLength
-        }
-    }
-    var square: Square {
-        willSet { // before set is triggered
-            triangle.sideLength = newValue.sideLength;
-        }
-    }
-
-    init(size: Double, name: String) {
-        square = Square(sideLength: size, name: name)
-        triangle = ETriangle(sideLength: size, name: name)
-    }
-}
-
-var tAnds = TriangleAndSquare(size: 10, name: "another test shape")
-print(tAnds.square.sideLength)
-print(tAnds.square.name)
-
+let ace = Rank.ace
+let aceRaw = ace.rawValue; // prints Int
+print(ace.simpleDesc()) // prints String
+print(ace)
