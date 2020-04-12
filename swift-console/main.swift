20a2a49e98ffc6a24c914f9b26a4677fb7274355
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-04-12 16:02:04 +0300

    Classes: getters and setters

--- swift-console/main.swift
+++ swift-console/main.swift
@@ -44,3 +44,30 @@ test.simpleDesc()
 
 print(test.area())
 print(test.simpleDesc())
+
+class ETriangle: NamedShape {
+    var sideLength: Double = 0;
+
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength;
+        super.init(name: name)
+        numberOfSides = 3;
+    }
+
+    var perimeter: Double {
+        get {
+            3.0 * sideLength;
+        } set {
+            sideLength = newValue / 3.0;
+        }
+    }
+    override func simpleDesc() -> String {
+        return "an equilateral triangle with sides of length \(sideLength)";
+    }
+}
+
+var triangle = ETriangle(sideLength: 3.1, name: "a tringle")
+print(triangle.perimeter)
+triangle.perimeter = 9.9
+print(triangle.sideLength)
+
