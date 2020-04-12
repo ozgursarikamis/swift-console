f85bc5ed4aa03f034c5e9ec8ed593840bb5f8636
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-04-12 20:34:04 +0300

    using Protocols as Types

--- swift-console/main.swift
+++ swift-console/main.swift
@@ -22,6 +22,33 @@ class SimpleClass: ExampleProtocol {
     }
 }
 
+class SimpleClass2: ExampleProtocol {
+    var simpleDesc: String = "a simple simple class"
+    var anotherProp: Int64 = 65
+    func adjust() {
+        simpleDesc += " , from adjust method";
+    }
+}
+
 var a = SimpleClass();
+var b = SimpleClass2();
+
 print(a.simpleDesc);
 a.adjust();
+
+
+extension Int: ExampleProtocol {
+    var simpleDesc: String {
+        return "the number \(self)"
+    }
+    mutating func adjust() {
+        self += 42
+    }
+}
+print(7.simpleDesc)
+
+print("------------------")
+let protocolValue: ExampleProtocol = a;
+let protocolValue2: ExampleProtocol = b;
+
+print(protocolValue.simpleDesc)
